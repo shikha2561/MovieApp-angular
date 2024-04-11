be31b4046ad08d7148605c241e8197b78d387851
shikha <kannushikha15@gmail.com>
2024-04-11 22:39:50 +0530

    Added project files

---
+++ src/app/shared/services/movie.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const options = {
+  params: {
+    include_adult: 'false',
+    include_video: 'true',
+    language: 'en-US',
+    page: '1',
+    sort_by: 'popularity.desc'
+  },
+  headers: { 
+  accept: 'application/json',
+  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOGRiNTExNGQxNzQxYzUxYjc2ODNlODI2ZDFlZDE4MiIsInN1YiI6IjY0MTJiMDc5ZTE4ZTNmMDg0MDNjODExYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X438_QQRZ2RwfKuudciLDgdRifkRsatrHimneAYcCMM'
+
+  }
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  http = inject(HttpClient);
+
+  getMovies() {
+    return this.http.get<any>('https://api.themoviedb.org/3/discover/movie', options)
+  }
+
+  getTvShows() {
+    return this.http.get('https://api.themoviedb.org/3/discover/tv', options)
+  }
+
+  // getRatedMovies() {
+    // return this.http.get('https://api.themoviedb.org/3/guest_session/guest_session_id/rated/movies', options)
+  // }
+
+  getBannerImage(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/images`, options)
+  }
+
+  getBannerVideo(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/videos`, options);
+  }
+
+  getBannerDetail(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}`, options);
+  }
+
+  getNowPlayingMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/now_playing', options)
+  }
+
+  getPopularMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/popular', options)
+  }
+
+  getTopRated() {
+    return this.http.get('https://api.themoviedb.org/3/movie/top_rated', options)
+  }
+
+  getUpcomingMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/upcoming', options)
+  }
+}
